f7150b0c8141b89fa175535590a01c7084b5acb6
szabofani <szabofani@users.noreply.github.com>
2020-04-15 19:16:42 +0200

    bencevagodé, tanulni fogok belöle

---
+++ script.js
@@ -0,0 +1,163 @@
+let data = {
+    photo: ['./Images/desert-rock.jpg', './Images/earth.jpg', './Images/solar-system.jpg', './Images/galaxy.jpg', './Images/galaxy-cluster.jpg', './Images/temple1.jpg', './Images/bamboo.jpg'],
+    title: ['The size of life', 'Our planet', 'The Solar System', 'Billions of solar systems', 'The enormous of enormous', 'Harmony', 'Inner peace'],
+    description: ['The size of life and the things around us can be overwhelming.',
+    'You sometimes can feel insignificant on a vast planet like Earth.',
+    'So let me introduce you our Solar System, where the sheer distances between planets are beyond imaginable.',
+    'Galaxies are consist of billions of solar systems, are you feeling small already?',
+    "This is a Galaxy Cluster, more billions of billions of planets and stars and asteroids out there, it is impossible and impractically selfish to think we are alone in a universe so vast, a human brain can't even think about it.",
+    'When humans think about such things as the universe, they can feel alone, and forget to think about things, that are much closer, still forgotten nowadays. That is Harmony.',
+    'Whit Harmony, there comes Inner Peace, which can contribute to a life that is full of empathy, and useful not just for you, but for others as well.'    
+]
+};
+let img = document.getElementById('img-container');
+let title = document.getElementById('image_title');
+let description = document.getElementById('image_description');
+let currentPhoto = 0;
+let up = document.getElementById('forward-arrow');
+let down = document.getElementById('back-arrow');
+let thumbnail_container = document.getElementById('thumbnails');
+let counter = 0;
+
+data.photo.forEach((i) => {
+    let newdiv = document.createElement('div');
+    newdiv.setAttribute('class', 'div'+counter+'');
+    newdiv.style.cssText = ('position: relative; text align: center; width: 80px; height: 80px, display: inline-block');
+    let thumbnail = new Image;
+    thumbnail.style.cssText = 'object-fit: cover; width: 70px; height: 70px; box-shadow: grey 0px 0px 3px 2px; padding: 3px; border-radius: 1px; position: relative';
+    thumbnail.setAttribute('id', counter);
+    //thumbnail.setAttribute('title', data.title[counter]);
+    let thumb_title = document.createElement('span');
+    thumb_title.setAttribute('id', 'thumb-title'+counter+'');
+    //thumb_title.style.cssText = 'position: relative; top: 0px; width: auto; text-align: center; background: black; padding: 10px, margin-bottom: 10px; color: white; visibility: hidden';
+    thumb_title.innerText = data.title[counter];
+    thumbnail.src = i;
+    counter++;
+    newdiv.appendChild(thumbnail);
+    newdiv.appendChild(thumb_title);
+    thumbnail_container.appendChild(newdiv);
+})
+
+let thumb0 = document.getElementById('0');
+let thumb1 = document.getElementById('1');
+let thumb2 = document.getElementById('2');
+let thumb3 = document.getElementById('3');
+let thumb4 = document.getElementById('4');
+let thumb5 = document.getElementById('5');
+let thumb6 = document.getElementById('6');
+
+function printImage(){
+    img.style.backgroundImage = 'url('+data.photo[currentPhoto]+')';
+    title.textContent = data.title[currentPhoto];
+    description.textContent = data.description[currentPhoto];
+    if (currentPhoto === 0) {
+        thumb0.style.background = 'rgb(65 179 247)';
+    } else {
+        thumb0.style.background = 'inherit';
+    }
+    if (currentPhoto === 1) {
+        thumb1.style.background = 'rgb(65 179 247)';
+    } else {
+        thumb1.style.background = 'inherit';
+    }
+    if (currentPhoto === 2) {
+        thumb2.style.background = 'rgb(65 179 247)';
+    } else {
+        thumb2.style.background = 'inherit';
+    }
+    if (currentPhoto === 3) {
+        thumb3.style.background = 'rgb(65 179 247)';
+    } else {
+        thumb3.style.background = 'inherit';
+    }
+    if (currentPhoto === 4) {
+        thumb4.style.background = 'rgb(65 179 247)';
+    } else {
+        thumb4.style.background = 'inherit';
+    }
+    if (currentPhoto === 5) {
+        thumb5.style.background = 'green';
+    } else {
+        thumb5.style.background = 'inherit';
+    }
+    if (currentPhoto === 6) {
+        thumb6.style.background = 'green';
+    } else {
+        thumb6.style.background = 'inherit';
+    }
+}
+
+printImage();
+
+up.onclick = function() {
+    if (currentPhoto === data.photo.length-1) {
+        currentPhoto = 0;
+        printImage();
+    } else {
+        currentPhoto++;
+        printImage();
+    }
+}
+
+down.onclick = function() {
+    if (currentPhoto === 0) {
+        currentPhoto = 6;
+        printImage();
+    } else {
+        currentPhoto--;
+        printImage();
+    }
+}
+
+
+thumb0.onclick = function() {
+    currentPhoto = 0;
+    printImage();
+}
+thumb1.onclick = function() {
+    currentPhoto = 1;
+    printImage();
+}
+thumb2.onclick = function() {
+    currentPhoto = 2;
+    printImage();
+}
+thumb3.onclick = function() {
+    currentPhoto = 3;
+    printImage();
+}
+thumb4.onclick = function() {
+    currentPhoto = 4;
+    printImage();
+}
+thumb5.onclick = function() {
+    currentPhoto = 5;
+    printImage();
+}
+thumb6.onclick = function() {
+    currentPhoto = 6;
+    printImage();
+}
+/*
+Failed attempts at syncing thumbnail and the image at the viewer area, making the viewer show the image clicked at the thumbnail section
+Also highlight the thumbnail of the image being showed at the viewer area
+let arr = [thumb0, thumb1, thumb2, thumb3, thumb4, thumb5, thumb6]
+arr.onclick = function() {
+    
+}
+thumb0.onclick = function() {
+    currentPhoto = thumb0;
+    printImage();
+}
+thumbnail_container.onclick = function() {
+    let ncounter = 0;
+    for (let n of arr) {
+        n.addEventListener('click', function() {
+            currentPhoto = ncounter;
+            printImage();
+            console.log(ncounter);
+        });
+        ncounter++;
+    }
+}
+*/
